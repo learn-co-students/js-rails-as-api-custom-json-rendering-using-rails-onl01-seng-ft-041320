211d315a187ace7c22bdeb7e0695e994747b5b5a
ReginaF2012 <skritschy@gmail.com>
2020-07-21 13:08:18 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -4,9 +4,14 @@ class BirdsController < ApplicationController
     # render json: birds, except: [:created_at, :updated_at]
     render json: birds, only: [:id, :name, :species]
   end
- 
+
   def show
     bird = Bird.find_by(id: params[:id])
-    render json: bird.slice(:id, :name, :species)
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
   end
+  
 end
